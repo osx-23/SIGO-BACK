7dd905ab6a8f5f6e65347aa84b0941eb988270d0
osx-23 <idatktl5@gmail.com>
2026-09-23 11:04:22 -0500

    fix(relevo): support evidence cleanup when removing via

--- src/main/java/com/sigo/relevo/infrastructure/persistence/repository/RelevoViaEvidenciaRepository.java
+++ src/main/java/com/sigo/relevo/infrastructure/persistence/repository/RelevoViaEvidenciaRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface RelevoViaEvidenciaRepository extends JpaRepository<RelevoViaEvidencia,Long> {
  List<RelevoViaEvidencia> findByRelevoViaIdOrderByIdAsc(Long relevoViaId);
+ void deleteByRelevoViaId(Long relevoViaId);
 }
