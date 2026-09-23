6392724dc3cda98c2b7f1d608d930616546a2821
osx-23 <idatktl5@gmail.com>
2026-09-23 00:26:59 -0500

    refactor(programacion): map validation errors at api boundary

--- src/main/java/com/sigo/programacion/api/controller/ProgramacionExceptionHandler.java
+++ src/main/java/com/sigo/programacion/api/controller/ProgramacionExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sigo.programacion.api.controller;
 
+import com.sigo.programacion.domain.ProgramacionValidationException;
 import com.sigo.programacion.domain.SecuenciaOrdenInvalidoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,17 @@ import java.util.Map;
         basePackageClasses = ProgramacionController.class
 )
 public class ProgramacionExceptionHandler {
+
+    @ExceptionHandler(ProgramacionValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleProgramacionValidation(
+            ProgramacionValidationException exception
+    ) {
+        return Map.of(
+                "message",
+                exception.getMessage()
+        );
+    }
 
     @ExceptionHandler(SecuenciaOrdenInvalidoException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
