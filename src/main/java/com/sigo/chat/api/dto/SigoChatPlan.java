377787c3161c7fead41b2774cd7bf8f6517cf655
osx-23 <idatktl5@gmail.com>
2026-09-23 09:38:52 -0500

    refactor(chat): remove internal models from api package

--- src/main/java/com/sigo/chat/api/dto/SigoChatPlan.java
+++
@@ -1,9 +0,0 @@
-package com.sigo.chat.api.dto;
-
-import java.util.List;
-
-public record SigoChatPlan(List<SigoToolRequest> tools) {
-    public List<SigoToolRequest> toolsSeguras() {
-        return tools == null ? List.of() : tools;
-    }
-}
