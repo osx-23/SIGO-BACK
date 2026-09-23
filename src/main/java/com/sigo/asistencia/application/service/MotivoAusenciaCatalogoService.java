b82015663cdcc73f781adfc002ae47b1a377633a
osx-23 <idatktl5@gmail.com>
2026-09-23 13:01:16 -0500

    perf(asistencia): cache absence reasons

--- src/main/java/com/sigo/asistencia/application/service/MotivoAusenciaCatalogoService.java
+++ src/main/java/com/sigo/asistencia/application/service/MotivoAusenciaCatalogoService.java
@@ -2,7 +2,9 @@ package com.sigo.asistencia.application.service;
 
 import com.sigo.asistencia.application.port.in.MotivoAusenciaCatalogoUseCase;
 import com.sigo.asistencia.application.port.out.MotivoAusenciaCatalogoPort;
+import com.sigo.shared.config.CacheConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,7 @@ public class MotivoAusenciaCatalogoService
     private final MotivoAusenciaCatalogoPort port;
 
     @Override
+    @Cacheable(CacheConfig.MOTIVOS_AUSENCIA)
     @Transactional(readOnly = true)
     public List<MotivoItem> listar() {
         return port.listar();
