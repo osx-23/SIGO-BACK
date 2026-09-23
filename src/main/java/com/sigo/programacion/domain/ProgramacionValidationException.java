7115180cabac4fd7acbfc4a48df6b32250b57182
osx-23 <idatktl5@gmail.com>
2026-09-23 00:26:14 -0500

    refactor(programacion): extract monthly shift use cases

---
+++ src/main/java/com/sigo/programacion/domain/ProgramacionValidationException.java
@@ -0,0 +1,8 @@
+package com.sigo.programacion.domain;
+
+public class ProgramacionValidationException extends RuntimeException {
+
+    public ProgramacionValidationException(String message) {
+        super(message);
+    }
+}
