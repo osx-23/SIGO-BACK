bec2cb2f88a18ae126ff1433314f1ca01a3c88b3
osx-23 <idatktl5@gmail.com>
2026-09-26 01:06:39 -0500

    perf(relevos): add batch repository queries

--- src/main/java/com/sigo/relevo/infrastructure/persistence/repository/RelevoChecklistRepository.java
+++ src/main/java/com/sigo/relevo/infrastructure/persistence/repository/RelevoChecklistRepository.java
@@ -1,7 +1,18 @@
 package com.sigo.relevo.infrastructure.persistence.repository;
+
 import com.sigo.relevo.infrastructure.persistence.entity.RelevoChecklist;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-public interface RelevoChecklistRepository extends JpaRepository<RelevoChecklist,Long> {
- List<RelevoChecklist> findByRelevoIdOrderByElementoCategoriaAscElementoOrdenAsc(Long relevoId);
+
+public interface RelevoChecklistRepository
+        extends JpaRepository<RelevoChecklist, Long> {
+
+    List<RelevoChecklist> findByRelevoIdOrderByElementoCategoriaAscElementoOrdenAsc(
+            Long relevoId
+    );
+
+    List<RelevoChecklist> findByRelevoIdInOrderByRelevoIdAscElementoCategoriaAscElementoOrdenAsc(
+            List<Long> relevoIds
+    );
 }
