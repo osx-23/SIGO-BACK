f1c172c4b265a77cf2842d429994517700fab571
osx-23 <idatktl5@gmail.com>
2026-09-26 12:29:28 -0500

    feat(personal): allow editing worker position

--- src/main/java/com/sigo/personal/api/dto/TrabajadorAdminUpdateRequest.java
+++ src/main/java/com/sigo/personal/api/dto/TrabajadorAdminUpdateRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotNull;
 
 public record TrabajadorAdminUpdateRequest(
         @NotNull Long plazaId,
+        @NotNull Long puestoId,
         @NotNull Boolean activo
 ) {}
