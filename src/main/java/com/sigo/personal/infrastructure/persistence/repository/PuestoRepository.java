ddff1538e6ba172644f7e50e6ca863c57da98a5f
osx-23 <idatktl5@gmail.com>
2026-09-26 12:18:15 -0500

    feat(personal): add puesto repository

---
+++ src/main/java/com/sigo/personal/infrastructure/persistence/repository/PuestoRepository.java
@@ -0,0 +1,14 @@
+package com.sigo.personal.infrastructure.persistence.repository;
+
+import com.sigo.personal.infrastructure.persistence.entity.Puesto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PuestoRepository
+        extends JpaRepository<Puesto, Long> {
+
+    List<Puesto> findByNombreStartingWithIgnoreCaseOrderByNombreAsc(
+            String prefijo
+    );
+}
