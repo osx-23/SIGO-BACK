dccd142bdec7d750c42cb34315c06f082bdb06c6
osx-23 <idatktl5@gmail.com>
2026-09-26 11:41:59 -0500

    feat(incidencias): query evidence by incidence

--- src/main/java/com/sigo/incidencia/infrastructure/persistence/repository/IncidenciaEvidenciaRepository.java
+++ src/main/java/com/sigo/incidencia/infrastructure/persistence/repository/IncidenciaEvidenciaRepository.java
@@ -4,6 +4,7 @@ import com.sigo.incidencia.infrastructure.persistence.entity.IncidenciaEvidencia
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IncidenciaEvidenciaRepository
         extends JpaRepository<IncidenciaEvidencia, Long> {
@@ -14,6 +15,12 @@ public interface IncidenciaEvidenciaRepository
     );
 
     long countByIncidenciaId(
+            Long incidenciaId
+    );
+
+    Optional<IncidenciaEvidencia>
+    findByIdAndIncidenciaId(
+            Long id,
             Long incidenciaId
     );
 }
